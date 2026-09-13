a7f228bf647625e24ed2713ca85f7838f6f36415
Thiago <thiagobarretocbv@hotmail.com>
2026-09-13 15:29:36 -0300

    example database

---
+++ src/NecroWiKi.Application/Models/Database/ExampleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NecroWiKi.Application.Models.Database
+{
+    [Table("EXAMPLE")]
+    public class ExampleModel
+    {
+        [Key]
+        [Column("ID")]
+        public long Id { get; set; }
+
+        [Column("NAME")]
+        public string? Name { get; set; }
+    }
+}
